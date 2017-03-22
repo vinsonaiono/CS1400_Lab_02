0796bbae60464f6add2966e739e611fe63fb284a
vinson aiono <vinson.aiono@gmail.com>
2017-03-22 03:23:06 -0600

    lab 02

---
+++ Lab_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lab_02
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+
+/*
+Question 1 - (D)
+Question 2 - (A)   
+*/
